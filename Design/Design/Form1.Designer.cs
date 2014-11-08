b15005ce1f524ec21dcae1141b9037d24070eb6e
sbuzoianu <stefan@buzoianu.ro>
2014-11-08 19:54:13 +0200

    Vremea C#

---
+++ Design/Design/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.weatherListItem2 = new WindowsFormsApplication1.WeatherListItem();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(716, 424);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(716, 319);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Celsius";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(716, 360);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kelvin";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(716, 398);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(87, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Fahrenheit";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 30;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // weatherListItem2
+            // 
+            this.weatherListItem2.BackColor = System.Drawing.Color.Transparent;
+            this.weatherListItem2.Location = new System.Drawing.Point(378, 210);
+            this.weatherListItem2.Name = "weatherListItem2";
+            this.weatherListItem2.Size = new System.Drawing.Size(332, 297);
+            this.weatherListItem2.TabIndex = 0;
+            this.weatherListItem2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.weatherListItem2_MouseDown);
+            this.weatherListItem2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.weatherListItem2_MouseMove);
+            this.weatherListItem2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.weatherListItem2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1112, 807);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.weatherListItem2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private WeatherListItem weatherListItem2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer3;
+
+    }
+}
+
---
+++ Design/Design/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+        }
+
+
+        public string oras;
+        public string gradeinitiale;
+        public float grade;
+        public float gradeK = 0;
+        public float gradeF;
+
+        public class Coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class Sys
+        {
+            public int type { get; set; }
+            public int id { get; set; }
+            public double message { get; set; }
+            public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class Main
+        {
+            public double temp { get; set; }
+            public double pressure { get; set; }
+            public int humidity { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+        }
+
+        public class Wind
+        {
+            public double speed { get; set; }
+            public double deg { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int all { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Coord coord { get; set; }
+            public Sys sys { get; set; }
+            public List<Weather> weather { get; set; }
+            public string @base { get; set; }
+            public Main main { get; set; }
+            public Wind wind { get; set; }
+            public Clouds clouds { get; set; }
+            public int dt { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public int cod { get; set; }
+        }
+        public class WeatherData
+        {
+            public Coord coord { get; set; }
+            public Sys sys { get; set; }
+            public Weather[] weather { get; set; }
+            public Main main { get; set; }
+            public Wind wind { get; set; }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Opacity += .05;
+            if (this.Opacity == 1)
+            {
+                timer1.Stop();
+            }
+        }
+
+        Rectangle rect_button1;
+        Rectangle rect_button2;
+        Rectangle rect_button3;
+        Rectangle rect_button4;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            rect_button1 = new Rectangle(265, 245, 79, 59);
+            rect_button2 = new Rectangle(265, 315, 79, 59);
+            rect_button3 = new Rectangle(265, 385, 79, 59);
+            rect_button4 = new Rectangle(725, 450, 69, 49);
+            radioButton1.Checked = true;
+
+            XmlTextReader reader = new XmlTextReader("http://freegeoip.net/xml/");
+
+            string city = "London";
+            string countrycode = "";
+            try
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                        if (reader.Name == "CountryCode")
+                        {
+                            reader.Read();
+                            countrycode = reader.Value;
+                        }
+
+                    if (reader.NodeType == XmlNodeType.Element)
+                        if (reader.Name == "City")
+                        {
+                            reader.Read();
+                            city = reader.Value;
+                        }
+                }
+            }
+            catch { }
+
+            textBox1.Text = city;
+            label1.Text = city;
+
+            try
+            {
+                oras = textBox1.Text;
+                textBox1.Clear();
+                WebClient webclient = new WebClient();
+                webclient.Encoding = Encoding.UTF8;
+                string json = webclient.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + oras + ",&lang=en&units=metric");
+                WeatherData data = JsonConvert.DeserializeObject<WeatherData>(json);
+                gradeinitiale = (Math.Round((decimal)data.main.temp, 2)).ToString();
+                grade = Convert.ToSingle(gradeinitiale);
+                weatherListItem2.Temperatura.Text = (Math.Round((decimal)data.main.temp, 2)).ToString() + "°C";
+                weatherListItem2.Presiune.Text = "Pressure: " + data.main.pressure.ToString();
+                weatherListItem2.Vant.Text = "Wind: " + data.wind.speed.ToString() + " km/h";
+                weatherListItem2.Umiditate.Text = "Humidity: " + data.main.humidity.ToString() + "%";
+                weatherListItem2.Descriere.Text = data.weather[0].description;
+
+
+                switch (data.weather[0].main)
+                {
+                    case "Clear": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clear;
+                        break;
+                    case "Clouds": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                        break;
+                    case "Fog": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                        break;
+                    case "light_clouds": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                        break;
+                    case "Drizzle": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                        break;
+                    case "Rain":
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                        break;
+                    case "Snow": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                        break;
+                    case "Storm": 
+                        this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                        break;
+                }
+            }
+
+            catch
+            {
+
+            }
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            
+
+        }
+
+        
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Font font = new Font("Arial", 16);
+            
+            
+            
+            e.Graphics.DrawImage(WindowsFormsApplication1.Properties.Resources.art_storm, rect_button1);
+            //e.Graphics.DrawString("Close", font, Brushes.Black, new Point(273, 260));
+
+            e.Graphics.DrawImage(WindowsFormsApplication1.Properties.Resources.art_rain, rect_button2);
+            //e.Graphics.DrawString("Min", font, Brushes.Black, new Point(273, 330));
+
+            e.Graphics.DrawImage(WindowsFormsApplication1.Properties.Resources.art_fog, rect_button3);
+            //e.Graphics.DrawString("Help", font, Brushes.Black, new Point(273, 400));
+
+            e.Graphics.DrawImage(WindowsFormsApplication1.Properties.Resources.art_snow, rect_button4);
+            //e.Graphics.DrawString("Search", font, Brushes.Black, new Point(725, 450));
+            
+            base.OnPaint(e);
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (rect_button1.Contains(e.Location))
+                timer2.Start();
+            if (rect_button2.Contains(e.Location))
+                this.WindowState = FormWindowState.Minimized;
+            if (rect_button3.Contains(e.Location))
+                MessageBox.Show("When you open the app you can see the weather in the place you are" + '\n' + "If you want to see the weather in other places, type the name of the place in the text box and then press Search!");
+            if (rect_button4.Contains(e.Location))
+            {
+                if (textBox1.TextLength != 0)
+                {
+                    timer3.Start();
+                   
+                    
+                }
+            }
+            base.OnMouseClick(e);
+        }
+
+        private Point mouseOffset;
+        private bool isMouseDown1 = false;
+        private bool isMouseDown2 = false;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset1;
+            int yOffset1;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset1 = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset1 = -e.Y - SystemInformation.CaptionHeight - SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset1, yOffset1);
+                isMouseDown1 = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown1)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown1 = false;
+            }
+        }
+
+        private void weatherListItem2_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset2;
+            int yOffset2;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset2 = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset2 = -e.Y - SystemInformation.CaptionHeight - SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset2, yOffset2);
+                isMouseDown2 = true;
+            }
+        }
+
+        private void weatherListItem2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown2)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void weatherListItem2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown2 = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Opacity -= .05;
+            if (this.Opacity == 0)
+            {
+                timer2.Stop();
+                Application.Exit();
+            }
+        }
+
+       
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer3.Start();
+            }
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            float numar = 273F;
+            gradeK = grade;
+            if (radioButton2.Checked == true && gradeK == grade)
+            {
+                gradeK = gradeK + numar;
+                weatherListItem2.Temperatura.Text = Convert.ToString(gradeK) + " K";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                weatherListItem2.Temperatura.Text = Convert.ToString(grade) + "°C";
+
+            }
+        }
+
+
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            gradeF = grade;
+
+            if (radioButton3.Checked == true && gradeF == grade)
+            {
+                gradeF = gradeF * 9 / 5 + 32;
+                weatherListItem2.Temperatura.Text = Convert.ToString(gradeF) + "°F";
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.Opacity -= .05;
+            if (this.Opacity == 0)
+            {
+                timer3.Stop();
+                try
+                {
+
+                    oras = textBox1.Text;
+                    label1.Text = textBox1.Text;
+                    textBox1.Clear();
+                    WebClient webclient = new WebClient();
+                    webclient.Encoding = Encoding.UTF8;
+                    string json = webclient.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=" + oras + ",&lang=en&units=metric");
+                    WeatherData data = JsonConvert.DeserializeObject<WeatherData>(json);
+                    gradeinitiale = (Math.Round((decimal)data.main.temp, 2)).ToString();
+                    grade = Convert.ToSingle(gradeinitiale);
+                    weatherListItem2.Temperatura.Text = (Math.Round((decimal)data.main.temp, 2)).ToString() + "°C";
+                    weatherListItem2.Presiune.Text = "Pressure: " + data.main.pressure.ToString();
+                    weatherListItem2.Vant.Text = "Wind: " + data.wind.speed.ToString() + " km/h";
+                    weatherListItem2.Umiditate.Text = "Humidity: " + data.main.humidity.ToString() + "%";
+                    weatherListItem2.Descriere.Text = data.weather[0].description;
+
+
+                    switch (data.weather[0].main)
+                    {
+                        case "Clear":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clear;
+                            break;
+                        case "Clouds":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                            break;
+                        case "Fog":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                            break;
+                        case "light_clouds":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_clouds;
+                            break;
+                        case "Drizzle":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                            break;
+                        case "Rain":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                            break;
+                        case "Snow":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                            break;
+                        case "Storm":
+                            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.art_light_rain;
+                            break;
+                    }
+                }
+
+                catch
+                {
+
+                }
+                this.BackgroundImageLayout = ImageLayout.Stretch;
+                timer1.Start();
+            }
+            
+        }
+    }
+}
